f832a06d0fe90838db94f3f40fc0847514dc78d4
piomuc000 <piomuc000@o365.student.pbs.edu.pl>
2023-04-20 14:54:28 +0200

    Initial commit

---
+++ app/src/main/java/pl/edu/pbs/apka10_smsy/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package pl.edu.pbs.apka10_smsy.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/pl/edu/pbs/apka10_smsy/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package pl.edu.pbs.apka10_smsy.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
